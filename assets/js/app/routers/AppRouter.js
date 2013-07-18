3285015399cae64b4232ec8c648baa60878869a0
phifnn <phifnn@gmail.com>
2013-07-18 15:29:11 -0700

    BB Integration

---
+++ assets/js/app/routers/AppRouter.js
@@ -0,0 +1,53 @@
+app.routers.AppRouter = Backbone.Router.extend({
+
+    routes: {
+        "": "home",
+        "challenge": "challenge",
+        "player": "player",
+        "stats": "stats"
+    },
+
+    initialize: function () {
+        app.slider = {};//new PageSlider($('body'));
+
+    },
+
+    home: function () {
+        // Since the home view never changes, we instantiate it and render it only once
+       // if (!app.homeView) {
+            app.homeView = new app.views.HomeView();
+            $('body').append(app.homeView.render().el);
+        /*} else {
+            console.log('reusing home view');
+            app.homeView.delegateEvents(); // delegate events when the view is recycled
+        }*/
+        //app.slider.slidePage(app.homeView.$el);
+    },
+
+    challenge: function (id) {
+        var employee = new app.models.Employee({id: id});
+        employee.fetch({
+            success: function (data) {
+                // Note that we could also 'recycle' the same instance of EmployeeFullView
+                // instead of creating new instances
+                app.slider.slidePage(new app.views.EmployeeView({model: data}).render().$el);
+            }
+        });
+    },
+
+    player: function (id) {
+        var employee = new app.models.Employee({id: id});
+        employee.fetch({
+            success: function (data) {
+                // Note that we could also 'recycle' the same instance of EmployeeFullView
+                // instead of creating new instances
+                app.slider.slidePage(new app.views.ReportsView({model: data}).render().$el);
+            }
+        });
+    },
+
+    stats: function (id) {
+        app.slider.slidePage(new app.views.MapView().render().$el);
+    }
+
+});
