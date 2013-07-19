fac4df0a9edb0f9d3add8e4eb37eb5d587a53661
phifnn <phifnn@gmail.com>
2013-07-19 13:52:52 -0700

    New changes

---
+++ assets/js/app/models/ChallengeModel.js
@@ -0,0 +1,14 @@
+app.models.Challenge = Backbone.Model.extend({
+});
+
+app.models.ChallengeCollection = Backbone.Collection.extend({
+    model: app.models.Challenge,
+    url: '/assets/data/seed1.json',
+    /*initialize:function(){
+        this.fetch();
+    },*/
+    parse:function(response){
+        //var data = parseUserData(response);
+        return response;
+    }
+});
